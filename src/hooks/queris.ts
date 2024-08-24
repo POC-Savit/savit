3af7109e481c9b0dbf9033c83598490c9fe0f471
dev-yongjaelee <dev.yongjaelee@cakecorp.com>
2024-08-25 04:45:41 +0900

    chore: 코드 린팅

--- src/hooks/queris.ts
+++ src/hooks/queris.ts
@@ -1,68 +1,69 @@
-
 import { Schema } from 'amplify/data/resource'
 import { generateClient } from 'aws-amplify/api'
-import { AllItem } from 'types/response/AllItem.type'
-import { Item } from 'types/Item.type'
 import { ItemType } from 'types/character.type'
-
+import { Item } from 'types/Item.type'
+import { AllItem } from 'types/response/AllItem.type'
 
 const client = generateClient<Schema>({
   authMode: 'userPool',
 })
 
-export const fetchUserInfo = () => client.models.UserInfo.list()
-    .then(resp => resp.data[0])
+export const fetchUserInfo = () =>
+  client.models.UserInfo.list().then((resp) => resp.data[0])
 
-export const fetchItems = () => client.queries.allItem({ authMode: 'apiKey'})
+export const fetchItems = () =>
+  client.queries
+    .allItem({ authMode: 'apiKey' })
     .then((resp) => JSON.parse(resp.data!!) as AllItem)
 
-type ItemWithOwn = Item & { isOwned: boolean, equipped: boolean }
+type ItemWithOwn = { isOwned: boolean; equipped: boolean } & Item
 
-export const fetchItemsWithUserinfo = () => Promise.all([fetchItems(), fetchUserInfo()])
-    .then(([items, userIfno]) => {
-        return [
-            ...items.face.map(item => ({
-                ...item,
-                isOwned: item.name in (userIfno.character?.own?.face ?? []),
-                equipped: item.name === userIfno.character?.current?.face,
-            }) as ItemWithOwn),
-            ...items.head.map(item => ({
-                ...item,
-                isOwned: item.name in (userIfno.character?.own?.head ?? []),
-                equipped: item.name === userIfno.character?.current?.head,
-            }) as ItemWithOwn),
-        ]
-    }) 
+export const fetchItemsWithUserinfo = () =>
+  Promise.all([fetchItems(), fetchUserInfo()]).then(([items, userIfno]) => {
+    return [
+      ...items.face.map(
+        (item) =>
+          ({
+            ...item,
+            isOwned: item.name in (userIfno.character?.own?.face ?? []),
+            equipped: item.name === userIfno.character?.current?.face,
+          }) as ItemWithOwn
+      ),
+      ...items.head.map(
+        (item) =>
+          ({
+            ...item,
+            isOwned: item.name in (userIfno.character?.own?.head ?? []),
+            equipped: item.name === userIfno.character?.current?.head,
+          }) as ItemWithOwn
+      ),
+    ]
+  })
 
-const hasItem = (userInfo: Schema["UserInfo"]["type"], item: ItemWithOwn) => {
-    const property = item.type === ItemType.FACE ? 'face' : 'head'
+const hasItem = (userInfo: Schema['UserInfo']['type'], item: ItemWithOwn) => {
+  const property = item.type === ItemType.FACE ? 'face' : 'head'
 
-    return new Set((userInfo.character?.own?.[property] ?? [])).has(item.name)
+  return new Set(userInfo.character?.own?.[property] ?? []).has(item.name)
 }
 
 export const buyItem = async (item: ItemWithOwn) => {
-    return fetchUserInfo()
-        .then((userInfo) => {
-            if (hasItem(userInfo, item) || 
-                item.price > userInfo.credit
-            ) return
-            
-            userInfo.credit -= item.price
-            const property = item.type === ItemType.FACE ? 'face' : 'head'
-            userInfo.character?.own?.[property]?.push(item.name)
-            return client.models.UserInfo.update(userInfo)
-        })
+  return fetchUserInfo().then((userInfo) => {
+    if (hasItem(userInfo, item) || item.price > userInfo.credit) return
+
+    userInfo.credit -= item.price
+    const property = item.type === ItemType.FACE ? 'face' : 'head'
+    userInfo.character?.own?.[property]?.push(item.name)
+    return client.models.UserInfo.update(userInfo)
+  })
 }
 
 export const equiItem = async (item: ItemWithOwn) => {
-    return fetchUserInfo() 
-    .then((userInfo) => {
-        if (!hasItem(userInfo, item)) return
-        
-        const property = item.type === ItemType.FACE ? 'face' : 'head'
-        if (userInfo.character?.current) userInfo.character!!.current!![property] = item.name
-        return client.models.UserInfo.update(userInfo)
-    })
+  return fetchUserInfo().then((userInfo) => {
+    if (!hasItem(userInfo, item)) return
+
+    const property = item.type === ItemType.FACE ? 'face' : 'head'
+    if (userInfo.character?.current)
+      userInfo.character!!.current!![property] = item.name
+    return client.models.UserInfo.update(userInfo)
+  })
 }
-
-
