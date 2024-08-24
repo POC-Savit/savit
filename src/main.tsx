e3ffbccc3cb4ee113889bed1fdac329384ffd9bf
Isaac Moon <dev.chanyongmoon@gmail.com>
2024-08-24 19:56:25 +0900

    ignore type for controlling auth config using cdk

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,8 @@ import App from './App.tsx'
 import './style/global.css'
 import '@stackflow/plugin-basic-ui/index.css'
 
+// ignore for controlling auth config using cdk 
+// @ts-ignore
 Amplify.configure(outputs)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
