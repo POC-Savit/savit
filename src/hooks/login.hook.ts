ae42508ee9ae72d663eef8fcc6735b811ca38551
dev-yongjaelee <dev.yongjaelee@cakecorp.com>
2024-08-24 21:14:22 +0900

    chore: 불필요한 코드 제거

--- src/hooks/login.hook.ts
+++ src/hooks/login.hook.ts
@@ -1,33 +1,32 @@
-import { Schema } from "amplify/data/resource"
-import { generateClient } from "aws-amplify/api"
-import { signIn, signUp, getCurrentUser } from "aws-amplify/auth"
-import { generate } from "random-words"
-import { useEffect, useState } from "react"
+import { Schema } from 'amplify/data/resource'
+import { generateClient } from 'aws-amplify/api'
+import { getCurrentUser, signIn, signUp } from 'aws-amplify/auth'
+import { generate } from 'random-words'
+import { useEffect, useState } from 'react'
 
 const client = generateClient<Schema>({
-    authMode: "iam",
-});
-  
+  authMode: 'iam',
+})
 
-const autoSignIn = (userName: string, password: string): Promise<boolean> => { 
-    return signUp({
-        username: userName,
-        password,
-    })
-    .then(() => signIn({username: userName, password }))
-    .then(resp => resp.isSignedIn)
+const autoSignIn = (userName: string, password: string): Promise<boolean> => {
+  return signUp({
+    username: userName,
+    password,
+  })
+    .then(() => signIn({ username: userName, password }))
+    .then((resp) => resp.isSignedIn)
 }
 
 export const useSignIn = (userName: string) => {
-    const [userInfo, setUser] = useState<Schema["UserInfo"]["type"]>()
-    const password = (generate(5) as string[]).join()
-    
-    useEffect(() => {
-        getCurrentUser()
-        .catch(() => autoSignIn(userName, password))
-        .then(() => client.models.UserInfo.list({ authMode: 'userPool'}))
-        .then(resp => setUser(resp.data[0]))
-    }, [])
+  const [userInfo, setUser] = useState<Schema['UserInfo']['type']>()
+  const password = (generate(5) as string[]).join()
 
-    return userInfo
+  useEffect(() => {
+    getCurrentUser()
+      .catch(() => autoSignIn(userName, password))
+      .then(() => client.models.UserInfo.list({ authMode: 'userPool' }))
+      .then((resp) => setUser(resp.data[0]))
+  }, [])
+
+  return userInfo
 }
