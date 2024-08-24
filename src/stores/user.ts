43cd3084edb50fe7a72fa711e0632f54cd1d0d32
dev-yongjaelee <dev.yongjaelee@cakecorp.com>
2024-08-25 08:24:03 +0900

    feat: 은행 계좌 연결 관련 코드 추가

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,3 +1,5 @@
+import type { Nullable, ObjectType } from 'type-util'
+
 import { Schema } from 'amplify/data/resource'
 import { atom } from 'jotai'
 
@@ -6,8 +8,13 @@ import { getCurrentUserInfo } from '~/hooks/login.hook'
 export const currentLevel = atom<number>(1)
 export const userInfo = atom<Schema['UserInfo']['type']>()
 export const credit = atom<number>(0)
+export const asset = atom<ObjectType[]>([])
 export const getUserInfo = atom(null, async (_get, set) => {
   const response = await getCurrentUserInfo()
   set(userInfo, response)
   set(credit, response.credit)
+  const currentAsset = JSON.parse(response.asset as string).savingAsset.map(
+    (item: ObjectType) => ({ ...item, title: '자유적금' })
+  )
+  set(asset, currentAsset)
 })
