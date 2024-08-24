f5123818ee0a752c23b69314de28e2c48cd0a1e5
Isaac Moon <dev.chanyongmoon@gmail.com>
2024-08-24 14:22:37 +0900

    add hook for fetching url of s3

---
+++ src/hooks/s3.hook.ts
@@ -0,0 +1,18 @@
+import { getUrl, GetUrlWithPathOutput } from "aws-amplify/storage"
+import { useEffect, useState } from "react"
+
+
+export const getObjectUrl = (path: string) => {
+    const [link, setLink] = useState<GetUrlWithPathOutput>()
+
+    useEffect(() => {
+        getUrl({
+          path,
+          options: {
+            expiresIn: 300,
+          }
+        }).then(setLink)
+      }, [])
+
+      return link
+}
