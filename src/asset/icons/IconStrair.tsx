aef8545d314dcb292d77db25c59e3875c2e635ed
Locke <hayeong28@naver.com>
2024-08-24 14:23:59 +0900

    feat: icons

---
+++ src/asset/icons/IconStrair.tsx
@@ -0,0 +1,28 @@
+import { SVGProps } from 'react'
+
+const IconStrair = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    fill="none"
+    height="24"
+    viewBox="0 0 24 24"
+    width="24"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <g clip-path="url(#clip0_215_2217)">
+      <path
+        d="M4 14.6667V20H20V4H14.6667V9.33333H9.33333V14.6667H4Z"
+        fill="currentColor"
+        stroke="currentColor"
+        stroke-width="1.5"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_215_2217">
+        <rect fill="currentColor" height="24" width="24" />
+      </clipPath>
+    </defs>
+  </svg>
+)
+
+export default IconStrair
