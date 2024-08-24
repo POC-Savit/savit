8b67ba929266d66a99c05d38509757cbe5510897
Isaac Moon <dev.chanyongmoon@gmail.com>
2024-08-25 06:29:42 +0900

    test

--- amplify/functions/item/handler.ts
+++ amplify/functions/item/handler.ts
@@ -1,9 +1,58 @@
 
 import type { Handler } from 'aws-lambda';
-import { AllItem } from '../../../types/response/AllItem.type'
+// import { AllItem } from '../../../types/response/AllItem.type'
 import { ItemType } from '../../../types/character.type';
+import { Amplify } from "aws-amplify";
+import output from '../../../amplify_outputs.json'
+// import {
+//     createUserInfo
+// } from "../graphql/mutations";
+import {
+    getUserInfo
+} from "../graphql/queries";
+import { generateClient } from 'aws-amplify/api';
+import { Schema } from '../../data/resource';
+
+// @ts-ignore
+Amplify.configure(output)
+// Amplify.configure(
+//     {
+//         API: {
+//           GraphQL: {
+//             endpoint: "",//env.AMPLIFY_DATA_GRAPHQL_ENDPOINT,
+//             region: env.AWS_REGION,
+//             defaultAuthMode: "iam",
+//           },
+//         },
+//       },
+//       {
+//         Auth: {
+//           credentialsProvider: {
+//             getCredentialsAndIdentityId: async () => ({
+//               credentials: {
+//                 accessKeyId: env.AWS_ACCESS_KEY_ID,
+//                 secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+//                 sessionToken: env.AWS_SESSION_TOKEN,
+//               },
+//             }),
+//             clearCredentialsAndIdentityId: () => {
+//               /* noop */
+//             },
+//           },
+//         },
+//       }
+//     );
 
 export const handler: Handler = async (event, context): Promise<string> => {
+
+    const client = generateClient<Schema>({});
+    const userInfo = await client.graphql({
+        query: getUserInfo,
+        variables: {
+            id: ''
+        },
+      });
+    console.log(userInfo)
     return JSON.stringify({
         head: [
             { name: 'BeachHat', price: 50, type: ItemType.HEAD},
