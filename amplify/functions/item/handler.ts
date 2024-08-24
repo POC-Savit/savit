af5ff34026c742756789afaddd829ef133b70cb4
Isaac Moon <dev.chanyongmoon@gmail.com>
2024-08-25 07:10:04 +0900

    test

--- amplify/functions/item/handler.ts
+++ amplify/functions/item/handler.ts
@@ -2,58 +2,58 @@
 import type { Handler } from 'aws-lambda';
 // import { AllItem } from '../../../types/response/AllItem.type'
 import { ItemType } from '../../../types/character.type';
-import { Amplify } from "aws-amplify";
-import output from '../../../amplify_outputs.json'
+// import { Amplify } from "aws-amplify";
+// import output from '../../../amplify_outputs.json'
 // import {
 //     createUserInfo
 // } from "../graphql/mutations";
-import {
-    getUserInfo
-} from "../graphql/queries";
-import { generateClient } from 'aws-amplify/api';
-import { Schema } from '../../data/resource';
+// import {
+//     getUserInfo
+// } from "../graphql/queries";
+// import { generateClient } from 'aws-amplify/api';
+// import { Schema } from '../../data/resource';
 
-Amplify.configure(
-    {
-      API: {
-        GraphQL: {
-          endpoint: process.env.AMPLIFY_DATA_GRAPHQL_ENDPOINT as string,
-          region: process.env.AWS_REGION,
-          defaultAuthMode: "iam",
-        },
-      },
-    },
-    {
-      Auth: {
-        credentialsProvider: {
-          getCredentialsAndIdentityId: async () => ({
-            credentials: {
-              accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
-              secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
-              sessionToken: process.env.AWS_SESSION_TOKEN as string,
-            },
-          }),
-          clearCredentialsAndIdentityId: () => {
-            /* noop */
-          },
-        },
-      },
-    },
-  );
+// Amplify.configure(
+//     {
+//       API: {
+//         GraphQL: {
+//           endpoint: process.env.AMPLIFY_DATA_GRAPHQL_ENDPOINT as string,
+//           region: process.env.AWS_REGION,
+//           defaultAuthMode: "iam",
+//         },
+//       },
+//     },
+//     {
+//       Auth: {
+//         credentialsProvider: {
+//           getCredentialsAndIdentityId: async () => ({
+//             credentials: {
+//               accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+//               secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+//               sessionToken: process.env.AWS_SESSION_TOKEN as string,
+//             },
+//           }),
+//           clearCredentialsAndIdentityId: () => {
+//             /* noop */
+//           },
+//         },
+//       },
+//     },
+//   );
   
 
 export const handler: Handler = async (event, context): Promise<string> => {
     console.log('start')
-    const client = generateClient<Schema>({});
-    console.log('start1')
-    const userInfo = await client.graphql({
-        query: getUserInfo,
-        variables: {
-            id: ''
-        },
-      });
-    console.log('start2')
-    console.log(userInfo)
+    // const client = generateClient<Schema>({});
+    // console.log('start1')
+    // const userInfo = await client.graphql({
+    //     query: getUserInfo,
+    //     variables: {
+    //         id: ''
+    //     },
+    //   }).catch(console.log);
+    // console.log('start2')
+    // console.log(userInfo)
     return JSON.stringify({
         head: [
             { name: 'BeachHat', price: 50, type: ItemType.HEAD},
