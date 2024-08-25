be8b11aae56d2c31075f0657bec363aeab18a152
dev-yongjaelee <dev.yongjaelee@cakecorp.com>
2024-08-25 09:50:00 +0900

    test

--- src/template/layer/Root.css.ts
+++ src/template/layer/Root.css.ts
@@ -1,4 +1,4 @@
-import { keyframes, style } from '@vanilla-extract/css'
+import { style } from '@vanilla-extract/css'
 
 import { button } from '~/components/common/MiniSquareButton.css'
 
@@ -68,19 +68,19 @@ export const text = style({
   letterSpacing: '-0.72px',
 })
 
-const shake = keyframes({
-  '0%': { transform: 'translate(1px, 1px) rotate(0deg);' },
-  '10%': { transform: 'translate(-1px, -2px) rotate(-1deg);' },
-  '100%': { transform: 'translate(1px, -2px) rotate(-1deg);' },
-  '20%': { transform: 'translate(-3px, 0px) rotate(1deg);' },
-  '30%': { transform: 'translate(3px, 2px) rotate(0deg);' },
-  '40%': { transform: 'translate(1px, -1px) rotate(1deg);' },
-  '50%': { transform: 'translate(-1px, 2px) rotate(-1deg);' },
-  '60%': { transform: 'translate(-3px, 1px) rotate(0deg);' },
-  '70%': { transform: 'translate(3px, 1px) rotate(-1deg);' },
-  '80%': { transform: 'translate(-1px, -1px) rotate(1deg);' },
-  '90%': { transform: 'translate(1px, 2px) rotate(0deg);' },
-})
+// const shake = keyframes({
+//   '0%': { transform: 'translate(1px, 1px) rotate(0deg);' },
+//   '10%': { transform: 'translate(-1px, -2px) rotate(-1deg);' },
+//   '100%': { transform: 'translate(1px, -2px) rotate(-1deg);' },
+//   '20%': { transform: 'translate(-3px, 0px) rotate(1deg);' },
+//   '30%': { transform: 'translate(3px, 2px) rotate(0deg);' },
+//   '40%': { transform: 'translate(1px, -1px) rotate(1deg);' },
+//   '50%': { transform: 'translate(-1px, 2px) rotate(-1deg);' },
+//   '60%': { transform: 'translate(-3px, 1px) rotate(0deg);' },
+//   '70%': { transform: 'translate(3px, 1px) rotate(-1deg);' },
+//   '80%': { transform: 'translate(-1px, -1px) rotate(1deg);' },
+//   '90%': { transform: 'translate(1px, 2px) rotate(0deg);' },
+// })
 
 export const stairShake = style([stair])
 
